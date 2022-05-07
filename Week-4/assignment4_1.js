63d0b5bc5b6a7369bf5c5594d11506e7f24991d6
Maheima <mahi.khandelwal28@gmail.com>
2022-05-08 00:58:21 +0530

    custom promise assignment update

--- Week-4/assignment4_1.js
+++ Week-4/assignment4_1.js
@@ -1,5 +1,47 @@
+const customPromiseState = {
+    PENDING: 'PENDING',
+    RESOLVED: 'RESOLVED',
+    REJECTED: 'REJECTED'
+}
+class customPromise {
+    constructor(fn){
+        this.customPromiseState = customPromiseState.PENDING
+        this.resolver = this.resolver.bind(this);
+        this.rejector = this.rejector.bind(this);
+        this.catchFn = null;
+        this.thenFn = null;
+        fn(this.resolver, this.rejector)
+    }
+
+
+    resolver(resolvedData){
+        if(this.customPromiseState === customPromiseState.PENDING){
+            this.thenFn && this.thenFn(resolvedData)
+        }
+        this.customPromiseState = customPromiseState.RESOLVED
+    }
+
+    rejector(rejectedData){
+        if (this.customPromiseState === customPromiseState.PENDING) {
+            this.catchFn && this.catchFn(rejectedData)
+        }
+        this.customPromiseState = customPromiseState.REJECTED
+       
+    }
+
+    then(thenFn) {
+        this.thenFn = thenFn;
+        return this
+    }
+
+    catch(catchFn){
+        this.catchFn = catchFn;
+        return this
+    }
+}
+
 const getNumber = () =>{
-    return new Promise((res,rej) => {
+    return new customPromise((res,rej) => {
         const randomNum = parseInt(Math.random() * 100 , 10);
         setTimeout(() => {
             if (randomNum % 5 === 0) {
